96fa5f2af3dd9dee7f84442d0cd78efeff6cba0a
jeanpokou <pokoujeanmarc@gmail.com>
2016-09-13 00:01:20 -0300

    adding config for neovim and autcomplete

--- init/brew.sh
+++ init/brew.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if test ! $(which brew); then 
+if test ! $(which brew); then
 	echo "installing homebrew"
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# update homebrew 
+# update homebrew
 
 brew update #make sure we using the lastest homebrew
 
@@ -17,7 +17,7 @@ echo -e "\n\n Installing wifi-password"
 brew install wifi-password # get wifi-password
 
 echo -e "\n\n Installing gnu-sed"
-brew install gnu-sed --with-defaults-names # Replace system sed by gnu-sed, replace words in files and so many more 
+brew install gnu-sed --with-defaults-names # Replace system sed by gnu-sed, replace words in files and so many more
 
 echo -e "\n\n Installing ack"
 brew install ack   # Search for pattern in current directory
@@ -35,10 +35,10 @@ brew tap caskroom/cask  #Install software with ease using cask
 
 #  Installing Development tools
 echo -e "\n\n Installing Development tools"
-brew install git  #  github 
+brew install git  #  github
 
 echo -e "\n\n Installing Development tools"
-brew install hub  # extend git features 
+brew install hub  # extend git features
 
 
 echo -e "\n\n Installing fzf "
@@ -58,7 +58,7 @@ brew install tmux  #Terminal multiplexer
 
 
 echo -e "\n\n Installing Zsh"
-brew install zsh # Zsh terminal 
+brew install zsh # Zsh terminal
 
 #Use zsh as default shell
 if ! fgrep -q 'usr/local/bin/zsh' /etc/shells; then
@@ -68,15 +68,15 @@ if ! fgrep -q 'usr/local/bin/zsh' /etc/shells; then
 fi;
 
 echo -e "\n\n Installing highlight"
-brew install highlight #syntax highlighter for several outputs formats 
+brew install highlight #syntax highlighter for several outputs formats
 
 
 echo -e "\n\n Installing nvm"
-brew install nvm #Node Version Manager 
+brew install nvm #Node Version Manager
 
 
 echo -e "\n\n Installing z"
-brew install z # jump around folder 
+brew install z # jump around folder
 
 
 echo -e "\n\n Installing markdown"
@@ -84,15 +84,15 @@ brew install markdown #Text to HTML conversion
 
 
 echo -e "\n\n Installing diff-so-fancy"
-brew install diff-so-fancy #git diff fancy display 
+brew install diff-so-fancy #git diff fancy display
 
 
 echo -e "\n\n Installing zsh-syntax-highlighting"
-brew install zsh-syntax-highlighting  #highlight syntax in zsh 
+brew install zsh-syntax-highlighting  #highlight syntax in zsh
 
 
 echo -e "\n\n Installing zsh-autosuggestions"
-brew install zsh-autosuggestions #Autosuggestion for zsh 
+brew install zsh-autosuggestions #Autosuggestion for zsh
 
 echo -e "\n\n Installing zplug, plugin package manager for zsh"
 curl -sL zplug.sh/installer | zsh
@@ -106,7 +106,7 @@ echo -e "\n\n Installing neo vim "
 brew install neovim/neovim/neovim  # enhance vim
 
 
-#Install font tools 
+#Install font tools
 
 # Installing operator mono from dropbox
 source ./operator-mono.sh
@@ -118,7 +118,7 @@ brew install homebrew/dupes/openssh # remote login tools
 
 
 # Install some CTF tools; see https://github.com/zardus/ctf-tools. inspired from mathiasbynens dotfiles
-brew install aircrack-ng	
+brew install aircrack-ng
 brew install bfg
 brew install binutils
 brew install binwalk
@@ -141,7 +141,7 @@ brew install tcpreplay
 brew install tcptrace
 brew install ucspi-tcp # `tcpserver` etc.
 #ask if user  want to install android-sdk as it takes  several minutes
-read -p "\n\n Do you want to install android-sdk? Notice: It will take several minutes to finish ? " 
+read -p "\n\n Do you want to install android-sdk? Notice: It will take several minutes to finish ? "
 echo "";
 if [[ $REPLY  =~ ^[Yy]$ ]]; then
 	brew install android-sdk;
